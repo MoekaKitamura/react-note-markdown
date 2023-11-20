ed5109015d0766da24d55e9d3016ec28ae30fab8
moekakitamura <kitamura@livesearch.co.jp>
2023-11-20 19:51:15 +0900

    add note function

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import "./Sidebar.css";
 
-const Sidebar = () => {
+const Sidebar = ({ onAddNote, notes }) => {
   return (
     <div className="appSidebar">
       <div className="appSidebarHeader">
         <h1>NOTE</h1>
-        <button>追加</button>
+        <button onClick={onAddNote}>追加</button>
       </div>
       <div className="appSidebarNotes">
-        <div className="appSidebarNote">
-          <div className="appSidebarNoteTitle">
-            <strong>タイトル</strong>
-            <button>削除</button>
+        {notes.map((note) => (
+          <div className="appSidebarNote" key={note.id}>
+            <div className="appSidebarNoteTitle">
+              <strong>{note.title}</strong>
+              <button>削除</button>
+            </div>
+            <p>{note.content}</p>
+            <small>
+              最終更新日：
+              {new Date(note.modDate).toLocaleDateString("ja-JP", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </small>
           </div>
-          <p>ノートの内容です</p>
-          <small>最終更新日：**/**</small>
-        </div>
+        ))}
       </div>
-      Sidebar
     </div>
   );
 };
