b3cdc7787d58cfa7962c187443f10cefdc5d7913
moekakitamura <kitamura@livesearch.co.jp>
2023-11-20 20:00:50 +0900

    generate unique ID using uuid library

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import { useState } from "react";
 import "./App.css";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
   const onAddNote = () => {
     const newNote = {
-      id: 1,
+      id: uuidv4(),
       title: "新しいノート",
       content: "新しいノートの内容",
       modDate: Date.now(),
     };
     setNotes([...notes, newNote]);
+    console.log(notes);
   };
 
   return (
