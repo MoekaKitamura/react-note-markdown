c3fd55657039b746f317ab31cffd66a1a6f2dce8
moekakitamura <kitamura@livesearch.co.jp>
2023-11-20 20:46:56 +0900

    main component design

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
 import "./Main.css";
 
 const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <div className="appMain">
+      <div className="appMainNoteEdit">
+        <input type="text" />
+        <textarea id="" placeholder="ノート内容を記入"></textarea>
+      </div>
+      <div className="appMainNotePreview">
+        <h1 className="previewTitle">タイトル</h1>
+        <div className="markdownPreview">プレビュー</div>
+      </div>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
